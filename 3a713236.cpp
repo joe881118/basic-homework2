146f690aaa26f757e8ba0b02cf7b65b8c05f08ad
joe881118 <48576616+joe881118@users.noreply.github.com>
2019-04-12 08:45:38 +0800

    Add files via upload

---
+++ 3a713236.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+
+int main() 
+{
+ int y,m,d;
+ printf("請輸入你的出生西元年：");
+ scanf("%d",&y); 
+ printf("請輸入你的出生月份：");
+ scanf("%d",&m);
+ printf("請輸入你的出生日期：");
+ scanf("%d",&d);
+ printf("你的出生年月日為：%d年%d月%d日\n",y,m,d);
+ system("pause");
+ return 0;
+}
